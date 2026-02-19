c3e683589e054d257d2696d17dca6fae5cb6784a
WahPlus <wahplus@proton.me>
2026-02-19 09:57:05 +0200

    Oops 2

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ ELEV=sudo
 
 $ELEV pacman -S --needed --noconfirm sway greetd greetd-regreet herbstluftwm xorg-server xorg-xinit kitty feh ttf-firacode-nerd fish fisher pkgfile
 # Set the command in the greetd config
-sed -i "s/^command.*\"$/command = \"sway --config /etc/greetd/sway-config\""
+$ELEV sed -i "s/^command.*\"\$/command = \"sway --config /etc/greetd/sway-config\"/gm" /etc/greetd/config.toml
 
 $ELEV echo $'exec "regreet; swaymsg exit"\ninclude /etc/sway/config.d/*' > /etc/greetd/sway-config
 
