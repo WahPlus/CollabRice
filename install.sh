91c156717fc7c6f40a171f4de6290d70aa3cc186
WahPlus <wahplus@proton.me>
2026-02-19 09:48:53 +0200

    Oops

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 # Sudo will be used for privilege elevation by default. If you would like to use another tool, change it here.
 ELEV=sudo
 
-$(ELEV) pacman -S --needed --noconfirm sway greetd greetd-regreet herbstluftwm xorg-server xorg-xinit kitty feh ttf-firacode-nerd fish fisher pkgfile
+$ELEV pacman -S --needed --noconfirm sway greetd greetd-regreet herbstluftwm xorg-server xorg-xinit kitty feh ttf-firacode-nerd fish fisher pkgfile
 # Set the command in the greetd config
 sed -i "s/^command.*\"$/command = \"sway --config /etc/greetd/sway-config\""
 
-$(ELEV) echo $'exec "regreet; swaymsg exit"\ninclude /etc/sway/config.d/*' > /etc/greetd/sway-config
+$ELEV echo $'exec "regreet; swaymsg exit"\ninclude /etc/sway/config.d/*' > /etc/greetd/sway-config
 
 rm -rf ~/.config/herbstluftwm ~/.config/fish ~/.config/kitty
 cp -r ./wallpaper.png herbstluftwm fish kitty ~/.config
 
-$(ELEV) systemctl enable --now greetd.service
+$ELEV systemctl enable --now greetd.service
